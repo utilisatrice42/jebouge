e1c7176bed4975cdf020f111c98c280d70e25aa3
Laura <l.belferroum@gmail.com>
2019-05-21 09:40:54 +0200

    changes

--- docs/components/footer.js
+++ docs/components/footer.js
@@ -12,20 +12,20 @@ render(){
 
         <footer className = "footerstyle">
         <div className = "credits">
-        <p>  - Site web : <a href = "https://lau-bel.github.io/cv-web/" target="_blank" rel="noopener noreferrer">Belferroum Laura </a>         
-        - Logo : <a target="_blank" href = "https://www.behance.net/LibertRemi?fbclid=IwAR1SjMhqlV6Vz1-R6i-8Oghmi9Na6xLdy_Zg8kgHAEvsnk5aRJcFAJmgZW0" rel="noopener noreferrer">Remi Libert</a> 
-        - Video (CC-BY-NC-ND) : <a href = "https://www.facebook.com/La-bulle-dAnnia-298859087392147/" target="_blank" rel="noopener noreferrer">La bulle d'Annia</a> 
+        <p>  - Site web : <a href = "https://lau-bel.github.io/cv-web/" target="_blank" rel="noopener">Belferroum Laura </a>         
+        - Logo : <a target="_blank" href = "https://www.behance.net/LibertRemi?fbclid=IwAR1SjMhqlV6Vz1-R6i-8Oghmi9Na6xLdy_Zg8kgHAEvsnk5aRJcFAJmgZW0" rel="noopener">Remi Libert</a> 
+        - Video (CC-BY-NC-ND) : <a href = "https://www.facebook.com/La-bulle-dAnnia-298859087392147/" target="_blank" rel="noopener">La bulle d'Annia</a> 
         <br/>
-        Site built with <a href = "https://www.gatsbyjs.org/" target="_blank" rel="noopener noreferrer">Gatsby</a> </p>
+        Site built with <a href = "https://www.gatsbyjs.org/" target="_blank" rel="noopener">Gatsby</a> </p>
         </div>
           <div className="textfooter" >
 
                 <li className = "collectif"> Collectif "Et ta soeur? "
-                <a href="https://www.instagram.com/collectifettasoeur/" target="_blank" rel="noopener noreferrer"><img src={instagram} className="instagram" alt="Instagramlogo"/></a>
-                <a href="https://www.facebook.com/collectifettasoeur/" target="_blank" rel="noopener noreferrer"><img src={facebook} className="facebook"  alt="Facebooklogo"/></a>
+                <a href="https://www.instagram.com/collectifettasoeur/" target="_blank" rel="noopener"><img src={instagram} className="instagram" alt="Instagramlogo"/></a>
+                <a href="https://www.facebook.com/collectifettasoeur/" target="_blank" rel="noopener"><img src={facebook} className="facebook"  alt="Facebooklogo"/></a>
                 </li>
                 <li> Vie féminine (Régionale Liège)
-                <a href="https://www.facebook.com/viefeminine.liegeregionale.3" target="_blank" rel="noopener noreferrer"><img src={facebook} className="facebook"  alt="Facebooklogo"/></a>
+                <a href="https://www.facebook.com/viefeminine.liegeregionale.3" target="_blank" rel="noopener"><img src={facebook} className="facebook"  alt="Facebooklogo"/></a>
                 </li>
 
           </div>
